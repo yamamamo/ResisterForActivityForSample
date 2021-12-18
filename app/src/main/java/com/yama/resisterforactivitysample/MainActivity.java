0227b95f30edd0cefc645780eec6251b5f47ccf8
yamamamo <84963245+yamamamo@users.noreply.github.com>
2021-12-18 18:32:25 +0900

    SubActivity 작업

--- app/src/main/java/com/yama/resisterforactivitysample/MainActivity.java
+++ app/src/main/java/com/yama/resisterforactivitysample/MainActivity.java
@@ -1,17 +1,58 @@
 package com.yama.resisterforactivitysample;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 /**
  * ResiserForActivitySample
  *
  * */
 public class MainActivity extends AppCompatActivity {
+    TextView tv_result;
+    Button btn_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+        tv_result = findViewById (R.id.tv_result);
+        btn_start = findViewById (R.id.btn_start);
+
+        btn_start.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext (), SubActivity.class);
+
+                launcher.launch(intent);
+            }
+        });
     }
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult> ()
+            {
+                @Override
+                public void onActivityResult(ActivityResult data)
+                {
+                        Log.d("TAG", "data : " + data);
+                    if (data.getResultCode() == Activity.RESULT_OK)
+                    {
+                        Intent intent = data.getData();
+                        String result = intent.getStringExtra ("result");
+
+                        tv_result.setText (result);
+                    }
+                }
+            });
 }
--- app/src/main/java/com/yama/resisterforactivitysample/SubActivity.java
+++ app/src/main/java/com/yama/resisterforactivitysample/SubActivity.java
@@ -2,13 +2,35 @@ package com.yama.resisterforactivitysample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SubActivity extends AppCompatActivity {
-
+    EditText edt_result;
+    Button btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_sub);
+
+         edt_result = findViewById (R.id.edt_result);
+         btn_back = findViewById (R.id.btn_back);
+
+         btn_back.setOnClickListener (view -> {
+             Intent intent = new Intent ();
+             String result = edt_result.getText ().toString ();
+             intent.putExtra ("result", result);
+             setResult (-1, intent);
+             finish ();
+
+         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed ();
     }
 }
